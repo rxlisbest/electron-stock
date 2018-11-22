89711b332f977d312232fd1eadc6b4a635a7f673
Roy <rxlisbest@gmail.com>
2018-11-22 16:42:07 +0800

    project init

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron-stock'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
