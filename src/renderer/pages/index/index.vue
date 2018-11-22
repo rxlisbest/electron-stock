6b5eaaf2fdc4ceb8773a07084684e86ec445cc05
Roy <rxlisbest@gmail.com>
2018-11-22 18:03:31 +0800

    import element

---
+++ src/renderer/pages/index/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <el-container>
+    <el-header></el-header>
+    <el-main>
+      123
+    </el-main>
+  </el-container>
+</template>
+
+<script>
+  import {
+    Container,
+    Header,
+    Main
+  } from 'element-ui'
+  export default {
+    name: 'landing-page',
+    components: {
+      Container,
+      Header,
+      Main
+    },
+    methods: {
+      open (link) {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
