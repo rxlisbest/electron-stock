1e94cf2852934f16359964070758d0f6c6e69710
Roy <rxlisbest@gmail.com>
2018-11-28 13:07:04 +0800

    数据库表结构设计

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import App from './App'
 import router from './router'
 import store from './store'
+import Migration from './migration'
 
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,12 +13,7 @@ Vue.use(Element, { size: 'small', zIndex: 3000 })
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-
-var sqlite3 = require('sqlite3').verbose()
-const path = require('path')
-console.log(path.join(__dirname, 'electron-stock.db'))
-var db = new sqlite3.Database(path.join(__dirname, 'electron-stock.db'))
-store.state.db = db
+store.state.sqlite3 = Migration.sqlite3
 /* eslint-disable no-new */
 new Vue({
   components: { App },
---
+++ src/renderer/migration.js
@@ -0,0 +1,50 @@
+var sqlite3 = require('sqlite3').verbose()
+const path = require('path')
+var db = new sqlite3.Database(path.join(__dirname, 'electron-stock.db'))
+
+// 创建表
+db.serialize(function () {
+  // 分类表
+  db.run('' +
+    'CREATE TABLE IF NOT EXISTS category (' +
+    'id INTEGER AUTO_INCREMENT,' +
+    'name VARCHAR(255),' +
+    'create_time BIGINT(20) NOT NULL DEFAULT 0,' +
+    'PRIMARY KEY(id)' +
+    ')')
+  // 商品表
+  db.run('' +
+    'CREATE TABLE IF NOT EXISTS goods (' +
+    'id INTEGER AUTO_INCREMENT,' +
+    'name VARCHAR(255),' +
+    'category_id INTEGER NOT NULL DEFAULT 0,' +
+    'price DECIMAL(8,2) NOT NULL DEFAULT 0.00,' +
+    'amount DECIMAL(8,2) NOT NULL DEFAULT 0.00,' +
+    'create_time BIGINT(20) NOT NULL DEFAULT 0,' +
+    'PRIMARY KEY(id)' +
+    ')')
+  // 订单表
+  db.run('' +
+    'CREATE TABLE IF NOT EXISTS `order` (' +
+    'id INTEGER AUTO_INCREMENT,' +
+    'name VARCHAR(255),' +
+    'goods_id INTEGER NOT NULL DEFAULT 0,' +
+    'price DECIMAL(8,2) NOT NULL DEFAULT 0.00,' +
+    'amount DECIMAL(8,2) NOT NULL DEFAULT 0.00,' +
+    'create_time BIGINT(20) NOT NULL DEFAULT 0,' +
+    'PRIMARY KEY(id)' +
+    ')')
+  // 用户表
+  db.run('' +
+    'CREATE TABLE IF NOT EXISTS user (' +
+    'id INTEGER AUTO_INCREMENT,' +
+    'name VARCHAR(255),' +
+    'phone VARCHAR(255),' +
+    'create_time BIGINT(20) NOT NULL DEFAULT 0,' +
+    'PRIMARY KEY(id)' +
+    ')')
+})
+
+export default {
+  sqlite3: sqlite3
+}
