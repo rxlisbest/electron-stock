7eb23f901cebed1869694ccae1ea55764ef58339
Roy <rxlisbest@gmail.com>
2018-12-08 13:41:31 +0800

    首页优化

--- src/renderer/pages/index/index.vue
+++ src/renderer/pages/index/index.vue
@@ -2,30 +2,30 @@
   <el-container>
     <el-header></el-header>
     <el-main>
-      <el-row :gutter="12">
-        <el-col :span="8">
+      <el-row :gutter="30">
+        <el-col :span="12">
           <el-card shadow="hover" @click.native="open('categories')">
-            <p><i class="el-icon-delete"></i></p>
+            <p><i class="el-icon-menu"></i></p>
             分类管理
           </el-card>
         </el-col>
-        <el-col :span="8">
+        <el-col :span="12">
           <el-card shadow="hover" @click.native="open('goods')">
             <p><i class="el-icon-goods"></i></p>
             商品管理
           </el-card>
         </el-col>
-        <el-col :span="8">
+      </el-row>
+      <el-row :gutter="30">
+        <el-col :span="12">
           <el-card shadow="hover" @click.native="open('users')">
-            <p><i class="el-icon-delete"></i></p>
+            <p><i class="el-icon-service"></i></p>
             客户管理
           </el-card>
         </el-col>
-      </el-row>
-      <el-row :gutter="12">
-        <el-col :span="8">
+        <el-col :span="12">
           <el-card shadow="hover" @click.native="open('orders')">
-            <p><i class="el-icon-delete"></i></p>
+            <p><i class="el-icon-sold-out"></i></p>
             订单管理
           </el-card>
         </el-col>
@@ -65,6 +65,8 @@
 <style>
   .el-card {
     cursor: pointer;
+    font-size: 30px;
+    text-align: center;
   }
   .el-col {
     text-align: center;
